5f28768822e807f7302ecc1dc2e89d5139c72fe3
Roman Tuomisto <romantuomisto@gmail.com>
2021-10-22 23:32:20 +0300

    fix search form logic

--- frontend/store/actions/adsActions.ts
+++ frontend/store/actions/adsActions.ts
@@ -2,7 +2,7 @@ import { Dispatch } from "react"
 import { getAds } from "../../utils/api"
 import { INIT_ADS } from "./actionTypes/adsActionTypes"
 
-export const initializeAds = (url: string) => {
+export const initializeAds = (url?: string) => {
   return async (dispatch: Dispatch<any>) => {
     const userAds = await getAds(url)
     dispatch({
