65e27d65a3a952fdd0ba6fe5126e31e0f6183c09
Roman Tuomisto <romantuomisto@gmail.com>
2021-11-19 13:24:55 +0200

    update workflow file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ yarn --frozen-lockfile
 pm2 restart all
 ENDSSH
 
-ssh -i ~/.ssh/doskaboard-backend.pem ec2-user@ec2-34-245-217-64.eu-west-1.compute.amazonaws.com << 'ENDSSH'
+ssh -i ~/.ssh/doskaboard-backend.pem ec2-user@ec2-18-202-29-197.eu-west-1.compute.amazonaws.com << 'ENDSSH'
 sudo yum update -y
 cd adboard
 git checkout .
